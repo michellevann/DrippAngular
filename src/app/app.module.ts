f69fb4689de1a9a412be4bc9081f5d9c47fe2ffb
lauracn15 <lauracn15@users.noreply.github.com>
2019-01-09 14:00:20 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { 
+  MatToolbarModule,  
+  MatButtonModule, 
+  MatFormFieldModule,
+  MatInputModule}
+  from '@angular/material';
+import { RegistrationComponent } from './components/registration/registration.component';
 
+const routes = [
+  { path: 'register', component: RegistrationComponent},
+  { path: '**', component: RegistrationComponent }
+];
+  
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+ 
 })
 export class AppModule { }
