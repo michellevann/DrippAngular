371d8dabeded8e5a728fdef99d05cb72d6582be7
Michelle Vann <michelle@dougvann.com>
2019-01-20 14:48:41 -0500

    formspree working

---
+++ src/app/models/Products.ts
@@ -0,0 +1,10 @@
+export interface Products {
+    PaintingEntityId: number;
+    Artist: string;
+    Title: string;
+    Size: string;
+    Color: string;
+    Price: number;
+    ImageUrl: string;
+    DateAdded?: Date;
+}
