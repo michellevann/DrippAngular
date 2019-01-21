625af37af89ad57b315fdf01193f5daaabf822fd
Michelle Vann <michelle@dougvann.com>
2019-01-21 14:24:11 -0500

    purchase index and delete working

--- src/app/components/purchase/purchase-index/purchase-index.component.ts
+++ src/app/components/purchase/purchase-index/purchase-index.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PurchaseService } from '../../../services/purchase.service';
 import { Purchase } from '../../../models/Purchase';
 import { MatTableDataSource } from '@angular/material';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-purchase-index',
@@ -9,15 +10,18 @@ import { MatTableDataSource } from '@angular/material';
   styleUrls: ['./purchase-index.component.css']
 })
 export class PurchaseIndexComponent implements OnInit {
+  _paintingService: any;
+  products$: any;
 
-  constructor(private _purchaseService: PurchaseService) { }
+  constructor(private _purchaseService: PurchaseService, private _productsService: ProductsService) { }
   
   ngOnInit() {
     this._purchaseService.getPurchases().subscribe((purchase: Purchase[]) => {
       this.dataSource = new MatTableDataSource<Purchase>(purchase);
+      console.log(this.dataSource)
     });
   }
-  columnNames = ['details', 'BuyerName', 'StreetAddress', 'AptNumber', 'City', 'State', 'Zip', 'BuyerEmail'];
+  columnNames = ['details', 'PurchaseDate', 'Title', 'Price', 'BuyerName', 'StreetAddress', 'City', 'State', 'Zip', 'BuyerEmail', 'buttons'];
   dataSource: MatTableDataSource<Purchase>
 }
 
