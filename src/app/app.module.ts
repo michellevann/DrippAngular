1f1525e9d22b65f935d4ff08bbfa4f6aacb7a845
Michelle Vann <michelle@dougvann.com>
2019-02-02 12:37:07 -0500

    Revert "built Contact & code cleanup"
    This reverts commit 462acc94617ad66934b30f1b2df53dd723c04af5.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,64 +1,65 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, ExtraOptions } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './components/login/login.component';
+import { JwtModule } from '@auth0/angular-jwt';
+
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { JwtModule } from '@auth0/angular-jwt';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { 
+  MatToolbarModule,  
   MatButtonModule, 
-  MatCardModule,
-  MAT_DIALOG_DEFAULT_OPTIONS,
-  MatDialogModule,
   MatFormFieldModule,
   MatInputModule,
   MatTableModule,
-  MatToolbarModule,  
+  MatCardModule,
+  MAT_DIALOG_DEFAULT_OPTIONS,
+  MatDialogModule,
   MatSnackBar,
   DateAdapter}
   from '@angular/material';
-import { AppComponent } from './app.component';
-import { ContactComponent } from './component/contact/contact.component';
-import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { PaintingCreateComponent } from './components/painting/painting-create/painting-create.component';
-import { PaintingDeleteComponent } from './components/painting/painting-delete/painting-delete.component';
-import { PaintingDetailComponent } from './components/painting/painting-detail/painting-detail.component';
-import { PaintingEditComponent } from './components/painting-edit/painting-edit.component';
-import { PaintingIndexComponent } from './components/painting/painting-index/painting-index.component';
-import { ProductsComponent, ProductsDialog } from './components/products/products.component';
-import { PurchaseDeleteComponent } from './components/purchase/purchase-delete/purchase-delete.component';
-import { PurchaseDetailComponent } from './components/purchase/purchase-detail/purchase-detail.component';
-import { PurchaseIndexComponent } from './components/purchase/purchase-index/purchase-index.component';
-import { AuthGuard } from './guards/auth.guard';
-import { AuthService } from './services/auth.service';
+
 import { PaintingService } from './services/painting.service';
 import { ProductsService } from './services/products.service';
+import { PaintingIndexComponent } from './components/painting/painting-index/painting-index.component';
+import { PaintingCreateComponent } from './components/painting/painting-create/painting-create.component';
+import { PaintingDetailComponent } from './components/painting/painting-detail/painting-detail.component';
+import { HomeComponent } from './components/home/home.component';
+import { PaintingEditComponent } from './components/painting-edit/painting-edit.component';
+import { PaintingDeleteComponent } from './components/painting/painting-delete/painting-delete.component';
+import { AuthGuard } from './guards/auth.guard';
+import { ProductsComponent, ProductsDialog } from './components/products/products.component';
 import { PurchaseService } from './services/purchase.service';
+import { PurchaseIndexComponent } from './components/purchase/purchase-index/purchase-index.component';
+import { PurchaseDetailComponent } from './components/purchase/purchase-detail/purchase-detail.component';
+import { PurchaseDeleteComponent } from './components/purchase/purchase-delete/purchase-delete.component';
+
+
 import { getLocaleDateTimeFormat } from '@angular/common';
 
+
 const routes = [
-  { path: 'admin', component: LoginComponent},
-  { path: 'contact', component: ContactComponent},
   { path: 'home', component: HomeComponent },
+  { path: 'admin', component: LoginComponent},
   { path: 'products', component: ProductsComponent },
 
-  { path: 'painting', canActivate: [AuthGuard], children: [
-    { path: '', component: PaintingIndexComponent},
-    { path: 'create', component: PaintingCreateComponent},
-    { path: 'delete/:id', component: PaintingDeleteComponent},
-    { path: 'detail/:id', component: PaintingDetailComponent},
-    { path: 'edit/:id', component: PaintingEditComponent}
-  ]},
-
   { path: 'purchase', canActivate: [AuthGuard], children: [
-    { path: '', component: PurchaseIndexComponent},
-    { path: 'delete/:id', component: PurchaseDeleteComponent},
-    { path: 'detail/:id', component: PurchaseDetailComponent}
+    {path: '', component: PurchaseIndexComponent},
+    {path: 'detail/:id', component: PurchaseDetailComponent},
+    {path: 'delete/:id', component: PurchaseDeleteComponent}
   ]},
-
+  { path: 'painting', canActivate: [AuthGuard], children: [
+     { path: '', component: PaintingIndexComponent},
+     { path: 'create', component: PaintingCreateComponent},
+     { path: 'edit/:id', component: PaintingEditComponent},
+     { path: 'detail/:id', component: PaintingDetailComponent},
+     { path: 'delete/:id', component: PaintingDeleteComponent}
+    ]},
   { path: '**', component: HomeComponent },
 ];
 
@@ -80,33 +81,34 @@ const routes = [
     PurchaseDeleteComponent
   ],
   imports: [
-    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    RouterModule.forRoot(routes),
     HttpClientModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatCardModule,
+    MatDialogModule,
     JwtModule.forRoot({
       config: {
         tokenGetter: tokenGetter
       }
-    }),
-    MatButtonModule,
-    MatCardModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatTableModule,
-    MatToolbarModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot(routes)
+    })
+ 
     //MatSnackBar
   ],
   
   providers: [
-    AuthGuard,
-    {provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: {hasBackdrop: false}},
     AuthService,
     PaintingService,
+    AuthGuard,
+    {provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: {hasBackdrop: false}},
     ProductsService,
     PurchaseService
   ],
